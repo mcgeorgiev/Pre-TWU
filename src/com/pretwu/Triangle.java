47ce5f39979a3c0665b7241dbed0abb9afc1d743
mcgeorgiev <michael_georgiev@hotmail.co.uk>
2018-02-23 20:56:45 +0000

    initial exercises

---
+++ src/com/pretwu/Triangle.java
@@ -0,0 +1,13 @@
+package com.pretwu;
+
+public class Triangle {
+    public void easiestExerciseEver() {
+        System.out.println("*");
+    }
+
+    public void drawHorizontalLine(Integer n) {
+        for (int i = 0; i < n; i++) {
+            System.out.print("*");
+        }
+    }
+}
